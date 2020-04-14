a024afac63639e696b71d0a2181e6e8e72c75bb4
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-04-13 23:09:20 -0300

    Creating the contact page layout

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import NavBar from '../../Components/NavBar/NavBar';
 
 import './Contact.css';
 
-import NavBar from '../../Components/NavBar/NavBar';
+//---ICONS---//
+import facebook from '../../Images/facebook.png';
+import instagram from '../../Images/instagram.png';
+import linkedin from '../../Images/linkedin.png';
+import cat from '../../Images/cat.png';
 
 class Contact extends React.Component {
     render() {
         return (
             <>
                 <div className="container">
-                    <div>
-                        <h1>Obrigado pela visita! Nos vemos por aí! :D</h1>
+                    <div className="title">
+                        <h1>Obrigado pela visita! Nos vemos por aí! <span>:D</span></h1>
+                    </div>
+                    <div className="contacts-container">
+                        <a href="" target="_blank">
+                            <img src={facebook} width={80} alt="facebook" />
+                        </a>
+                        <a href="" target="_blank">
+                            <img src={instagram} width={80} alt="instagram" />
+                        </a>
+                        <a href="" target="_blank">
+                            <img src={linkedin} width={80} alt="linkedin" />
+                        </a>
+                        <a href="" target="_blank">
+                            <img src={cat} width={80} alt="github" />
+                        </a>
+                    </div>
+                    <div className="identity">
+                        <span>Michel de Oliveira Nascimento - Desenvolvedor Front End</span>
                     </div>
                 </div>
                 <NavBar />
