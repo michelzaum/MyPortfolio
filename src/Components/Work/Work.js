b46f78b3883cd75e12ab4e7d7e4ac5452e8452ff
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-04-14 21:33:21 -0300

    Usage of the useState and useEffect to implement the works

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 
 import './Work.css'
 
-function Work() {
-    return (
-        <>
-            <Link to="" className="work-item">
-                <div className="work-title-container">
-                    <h1>Titulo Work</h1>
-                </div>
-                <div className="work-description-container">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, iusto <br />
-                        sint, eos animi quos neque accusantium ad alias labore dicta sed rem exercitationem<br />
-                         quas? Et ipsam earum omnis dicta consequuntur?</p>
-                </div>
-                <div className="work-techs-container">
-                    <span>ReactJS, React Native, NodeJS</span>
-                </div>
-            </Link>
-        </>
-    )
+class Work extends React.Component {
+    render() {
+        return (
+            <>
+                <Link to="" className="work-item">
+                    <div className="work-title-container">
+                        <h1>{this.props.title}</h1>
+                    </div>
+                    <div className="work-description-container">
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className="work-techs-container">
+                        <span>{this.props.techs}</span>
+                    </div>
+                </Link>
+            </>
+        )
+    }
+
 }
 
 export default Work;
--- src/Pages/Works/Works.js
+++ src/Pages/Works/Works.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Works.css';
 import NavBar from '../../Components/NavBar/NavBar';
 import Work from '../../Components/Work/Work';
 
 function Works() {
-        return (
-            <>
-                <div className="title-works-container">
-                    <h1>Meus trabalhos</h1>
-                </div>
-                <div className="works-container">
-                    <Work />
-                    <Work />
-                    <Work />
-                    <Work />
-                    <Work />
-                    <Work />
-                    <Work />
-                </div>
-                <NavBar />
-            </>
-        )
+    let [work, setWork] = useState([]);
+
+    let data = [
+        { title: "StreamIt", description: "A mais nova rede social para programadores!", techs: "ReactJS, React Native, NodeJS, MongoDB" },
+        { title: "My Portfólio", description: "Um site para que você possa conhecer um pouco mais sobre mim", techs: "ReactJS, CSS" },
+        { title: "iFigaro", description: "Deixe a tecnologia te ajudar na hora de dar aquele toque no visual ;)", techs: "React Native, NodeJS, CSS" }
+    ];
+
+    useEffect(() => {
+        setWork(data)
+    }, []);
+
+    return (
+        <>
+            <div className="title-works-container">
+                <h1>Meus trabalhos</h1>
+            </div>
+            <div className="works-container">
+                {work.map(item => (
+                    <Work
+                        title={item.title}
+                        description={item.description}
+                        techs={item.techs}
+                    />
+                ))}
+            </div>
+            <NavBar />
+        </>
+    )
 }
 
 export default Works;
